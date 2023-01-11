19cdc79853fe6c4238bf226a04803908fcd665e6
Farida-Hamid <faridaaddala@gmail.com>
2023-01-11 15:50:58 +0300

    Add validations

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,7 @@ class Group < ApplicationRecord
   belongs_to :user, class_name: "User", foreign_key: "author_id"
   has_and_belongs_to_many :payments
 
+  validates :name, presence: true
+  validates :icon, presence: true
+
 end
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -2,4 +2,9 @@ class Payment < ApplicationRecord
   belongs_to :user, class_name: "User", foreign_key: "author_id"
   has_and_belongs_to_many :groups
 
+  validates :name, presence: true
+  validates :amount, presence: true, numericality: { greater_than: 0 }
+  validates :groups, presence: true
+
+
 end
