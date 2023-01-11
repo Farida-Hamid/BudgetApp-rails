1e99dee98dbcf7b243e044b80d74893680c9a754
Farida-Hamid <faridaaddala@gmail.com>
2023-01-11 17:04:45 +0300

    Add devise to authenticate users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :trackable
+
   has_many :groups, class_name: "Group", foreign_key: "author_id"
   has_many :payments, class_name: "Payment", foreign_key: "author_id"
 
