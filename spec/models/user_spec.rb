b4f5951dc5a0417076918ba7c2cb9c896a86a200
Farida-Hamid <faridaaddala@gmail.com>
2023-01-13 16:41:21 +0300

    Test group model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'validation test' do
+  describe 'validate user test' do
     it 'validate name' do
       subject { User.new(email: 'Shady@gmail.com', password: '123456', password_confirmation: '123456')}
       expect(subject).to_not be_valid
     end
 
-    it 'validate name' do
+    it 'validate email' do
       subject { User.new(name: 'Shady', password: '123456', password_confirmation: '123456')}
       expect(subject).to_not be_valid
     end
 
-    it 'validate name' do
+    it 'validate password' do
       subject { User.new(name: 'Shady', email: 'Shady@gmail.com')}
       expect(subject).to_not be_valid
     end
